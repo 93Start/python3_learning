0795f88052419ddfbaa40283a421a223aa0972bc
93Start <S.poolpong93@gmail.com>
2021-01-25 17:09:34 +0700

    ep3.py print

---
+++ ep3.py
@@ -0,0 +1,24 @@
+# string => ข้อความ ตัวอักษร ' "" '''
+print('Hello world')
+print("Hello world")
+print('''Hello world''')
+
+# number => ตัวเลข -1 0 1 3.99
+print(-1)
+print(0)
+print(1)
+print(3.99)
+
+# boolean => true false
+print(True)
+print(False)
+
+# print function
+print("Suphachok \n")
+print("Suphachok \t")
+print("Suphachok \n"+"learn python")
+print("Suphachok \t"+"learn python")
+print(1+9)
+print("1"+"9")
+
+
