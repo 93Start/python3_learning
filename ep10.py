c89772a73b766e093de63e9514a75b872be07f13
93Start <S.poolpong93@gmail.com>
2021-01-30 16:15:46 +0700

    ep9&10

---
+++ ep10.py
@@ -0,0 +1,18 @@
+# Logic operator
+# and,or,not
+# result True or False
+
+x = (5<10)
+y = (10==5)
+
+print(x)
+print(y)
+
+z = x and y
+print(z)
+
+z = x or y
+print(z)
+
+z = x or y
+print(not z)
---
+++ ep9.py
@@ -0,0 +1,12 @@
+# compare operator
+# ข้อมูลเหมือนกัน
+# result = True or False
+
+x,y = 10, 5
+
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
