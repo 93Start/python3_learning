e875e7e2748e20c18fe1eb4294f48fe6e1907f51
93Start <S.poolpong93@gmail.com>
2021-01-30 13:40:06 +0700

    ep5.py data & variable

---
+++ ep4.py
@@ -0,0 +1,17 @@
+# datatype & variable
+# integer           | int   | 3 300 200 
+# floating point    | float | 3.14 5.23 100.0
+# String            | str   | "test" 'test'
+# list              | list  | [10,"hello",200.3]
+# Dictionaries      | dict  | {"mykey":"value","name":"93Start"}
+# Tuples            | tup   | (10,"hello",200.3)
+# Sets              | set   | ("a","b")
+# Booleans          | bool  | True or False
+
+# ชื่อตัวแปร  = ค่าของตัวแปร    
+x = 10 #integer
+print(x)
+
+print("ผลลัพท์" + str(x)) #แปลง int เป็น String โดยใช้ str
+
+
